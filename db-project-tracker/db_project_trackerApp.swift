a65dcf09d9fb67ba3a0c9d672589f7e1082b7e76
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-09 16:01:39 +0300

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Omer Cagri Sayir on 9.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
