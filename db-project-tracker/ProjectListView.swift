55a64131697d7fc638f464a57604c72d3b67cad0
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-11 14:56:39 +0300

    - background linear gradient added - button style added but not complete

--- db-project-tracker/ProjectListView.swift
+++ db-project-tracker/ProjectListView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct ProjectListView: View {
     var body: some View {
-        VStack {
-            Image("cross")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .font(Font.bigHeadline)
+        ZStack {
+            LinearGradient(colors: [Color("Deep Purple"), Color("Blush Pink")], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+
+            VStack(alignment: .leading) {
+                Text("Projects")
+                    .font(Font.screenHeading)
+                    .foregroundStyle(.white)
+
+                ScrollView {
+                }
+
+                Button(action: {
+                    // Todo
+                }, label: {
+                    Image("cross")
+                })
+            }.padding()
         }
-        .padding()
     }
 }
 
