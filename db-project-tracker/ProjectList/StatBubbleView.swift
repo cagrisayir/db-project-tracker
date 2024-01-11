2e8646563cc25e61e68f30855726c5d233d946a9
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-11 15:23:58 +0300

    changed color type

--- db-project-tracker/ProjectList/StatBubbleView.swift
+++ db-project-tracker/ProjectList/StatBubbleView.swift
@@ -10,13 +10,13 @@ import SwiftUI
 struct StatBubbleView: View {
     var title: String = "Hours"
     var stat: String = "290"
-    var startColor: String = "Navy"
-    var endColor: String = "Blue"
+    var startColor = Color("Navy")
+    var endColor = Color("Blue")
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 15)
-                .fill(LinearGradient(colors: [Color(startColor), Color(endColor)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .fill(LinearGradient(colors: [startColor, endColor], startPoint: .topLeading, endPoint: .bottomTrailing)
                 )
 
             VStack {
