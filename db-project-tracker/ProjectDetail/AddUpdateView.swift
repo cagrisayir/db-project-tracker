9015c79272568bbafaec4c98e9419f37935898e4
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-15 19:17:44 +0300

    add an update view added

---
+++ db-project-tracker/ProjectDetail/AddUpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  AddUpdateView.swift
+//  db-project-tracker
+//
+//  Created by Omer Cagri Sayir on 15.01.2024.
+//
+
+import SwiftUI
+
+struct AddUpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddUpdateView()
+}
